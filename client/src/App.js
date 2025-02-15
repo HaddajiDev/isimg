aef439c059da698c4b12766027e75574a28d47fe
HaddajiDev <ahmedhaddajiahmed@gmail.com>
2025-02-15 15:38:44 +0100

    lsim2

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './comps/Home';
 import LSMI1 from './comps/LSIM1';
+import LSIM2 from './comps/LSIM2';
 
 function App() {
   return (
     <div>
-      <LSMI1 />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/lsim1' element={<LSMI1 />}/>
+        <Route path='/lsim2' element={<LSIM2 />}/>
+      </Routes>
+      
     </div>
   );
 }
