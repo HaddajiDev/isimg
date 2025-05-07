03175b7f443b710c0d251442c290f9acb7070431
HaddajiDev <ahmedhaddajiahmed@gmail.com>
2025-05-07 17:49:29 +0100

    fix

--- api/index.js
+++ api/index.js
@@ -14,10 +14,15 @@ app.use(express.urlencoded({ extended: true }));
 
 
 const corsOptions = {
-    origin: ['http://localhost:3000', 'https://isimg.vercel.app', "https://isimg-preview.vercel.app", "https://isimg-pre-back.vercel.app"],
+    origin: [
+        'http://localhost:3000', 
+        'https://isimg.vercel.app',
+        'https://isimg-preview.vercel.app',
+        'https://isimg-pre-back.vercel.app'
+    ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Range']
 };
 
 app.use(cors(corsOptions));
